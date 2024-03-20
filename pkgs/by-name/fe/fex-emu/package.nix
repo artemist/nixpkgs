e16a3e1ced75d335617147fb5e8f6f60b4f81fc1
Artemis Tosini <me@artem.ist>
2024-03-20 03:30:14 +0000

    fex-emu: thunks partially build

--- pkgs/by-name/fe/fex-emu/package.nix
+++ pkgs/by-name/fe/fex-emu/package.nix
@@ -5,17 +5,28 @@
   cmake,
   ninja,
   pkg-config,
+  pkgsCross,
   nasm,
   python3,
   SDL2,
   libepoxy,
   openssl,
-  squashfsTools,
-  squashfuse,
+  libffi,
+  ncurses,
+  libxml2,
+  xorg,
+  zlib,
+  libglvnd,
+  alsa-lib,
+  libdrm,
+  wayland,
+  vulkan-headers,
+  buildThunks ? true,
 }:
 
 let
   stdenv = llvmPackages.stdenv;
+  crossGcc = pkgsCross.gnu64.buildPackages.gcc;
 in
 stdenv.mkDerivation rec {
   pname = "fex-emu";
@@ -45,20 +56,54 @@ stdenv.mkDerivation rec {
     SDL2
     libepoxy
     openssl
-    squashfuse
-    squashfsTools
+    libffi
+    ncurses
+    libxml2
+    zlib
+  ] ++ lib.optionals buildThunks [
+    alsa-lib
+    libdrm
+    libglvnd
+    vulkan-headers
+    wayland
+    xorg.libX11
+    xorg.libXau
+    xorg.libXdmcp
+    xorg.libXext
+    xorg.libXrender
+    xorg.libXfixes
+    xorg.libxcb
+    xorg.libxshmfence
   ];
 
   preConfigure = ''
     export AR=llvm-ar
     export RANLIB=llvm-ranlib
     export STRIP=llvm-strip
+  '' + lib.optionalString buildThunks ''
+    substituteInPlace toolchain_x86_{32,64}.cmake \
+      --replace x86_64-linux-gnu- ${crossGcc}/bin/${crossGcc.targetPrefix}
   '';
+
+  env = lib.optionalAttrs buildThunks {
+    # libclang in thunkgen requires llvm includes
+    # Ordinarily, this would be in depsBuildBuild but
+    # doing so sets our compiler to be clang-unwrapped,
+    # so put the include here.
+    NIX_CFLAGS_COMPILE = "-I${llvmPackages.libllvm.dev}/include";
+  };
 
   cmakeFlags = [
     "-DOVERRIDE_VERSION=${version}"
     "-DENABLE_LTO=True"
+  ] ++ lib.optionals buildThunks [
+    "-DBUILD_THUNKS=True"
+    # For thunk generation
+    "-DClang_DIR=${llvmPackages.libclang.dev}/lib/cmake/clang"
+    "-DLLVM_DIR=${llvmPackages.libllvm.dev}/lib/cmake/llvm"
   ];
+
+  doCheck = true;
 
   meta = with lib; {
     description = "A fast usermode x86 and x86-64 emulator for Arm64 Linux";
